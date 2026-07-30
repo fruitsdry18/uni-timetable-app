10dbeca4ad03058be4c8b14c43eab24bfcb3b3a1
fruitsdry18 <fruitsdry18@gmail.com>
2026-07-30 11:35:41 +0900

    変更内容のメモ（例：デザインの修正）

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,36 +1,52 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const login = () => {
+const Login = () => {
   return (
     <div className="w-full">
-      <h1 className="mt-30 text-center text-3xl text-blue-500 font-bold">UNI・KANRI</h1>
-      <div className="mt-7 bg-blue-100 rounded-2xl h-auto rounded-2xl max-w-sm pt-10 mx-10">
+      <h1 className="mt-30 text-center text-3xl text-blue-500 font-bold">
+        UNI・KANRI
+      </h1>
+      <div className="mt-7 bg-blue-100 rounded-2xl h-auto max-w-sm pt-10 px-5 pb-5 mx-auto">
         <h2 className="font-bold text-center text-black text-2xl">ログイン</h2>
-        <h3 className="mt-10 ml-5">メールアドレス：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="email"
-            inputMode="email"
-            placeholder="example@univ.ac.jp"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">パスワード：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="password"
-            placeholder="パスワードを入力"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <div className="text-right pb-5 mr-5 ">
-          <Link to="/registration" className="text-xs text-gray-700">
-            登録がお済みでない方へ
-          </Link>
-        </div>
+
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h3 className="mt-10 ml-5">メールアドレス：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="email"
+              inputMode="email"
+              placeholder="example@univ.ac.jp"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">パスワード：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="password"
+              placeholder="パスワードを入力"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <div className="text-right pb-5 mr-5">
+            <Link to="/registration" className="text-xs text-gray-700">
+              登録がお済みでない方へ
+            </Link>
+          </div>
+
+          <div className="bg-blue-500 hover:bg-blue-600 transition-colors mx-20 rounded-xl py-1 mb-10">
+            <button
+              type="submit"
+              className="text-3xl font-bold w-full text-white cursor-pointer"
+            >
+              ログイン
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
 };
 
-export default login;
+export default Login;
--- src/components/registration.tsx
+++ src/components/registration.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Registration = () => {
   return (
-    <div className="">
+    <div className="w-full">
       <h1 className="mt-30 text-center text-3xl text-blue-500 font-bold">
         UNI・KANRI
       </h1>
-      <div className="mt-7 bg-blue-100 rounded-2xl h-auto rounded-2xl max-w-sm py-10 mx-10 mb-30">
+      <div className="mt-7 bg-blue-100 rounded-2xl h-auto max-w-sm py-10 px-5 mx-auto mb-30">
         <h2 className="font-bold text-center text-black text-2xl">
           新規会員登録
         </h2>
@@ -15,79 +15,95 @@ const Registration = () => {
             ログインはこちら
           </Link>
         </div>
-        <h3 className="mt-5 ml-5">使用するメールアドレス：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="email"
-            inputMode="email"
-            placeholder="example@univ.ac.jp"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">パスワードの設定：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="password"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">パスワードを再度入力：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="password"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">フルネーム：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="text"
-            placeholder="山田太郎"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">ニックネーム：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="text"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">学校名：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="text"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">学年：</h3>
-        <select className="my-2 mx-5 bg-gray-100 rounded xl">
-          <option value="">選択してください</option>
-          <option value="1">1年</option>
-          <option value="2">2年</option>
-          <option value="3">3年</option>
-          <option value="4">4年</option>
-        </select>
-        <h3 className=" ml-5">学部：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="text"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
-        <h3 className=" ml-5">学科：</h3>
-        <div className=" my-2 mx-5">
-          <input
-            type="text"
-            className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base"
-          />
-        </div>
 
-        <div className="bg-blue-500 mx-20 rounded-xl py-1">
-          <button className="text-3xl font-bold w-full text-white hover:bg-blue-600 transition active:scale-[0.95]">
-            登録
-          </button>
-        </div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h3 className="mt-5 ml-5">使用するメールアドレス：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="email"
+              inputMode="email"
+              placeholder="example@univ.ac.jp"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">パスワードの設定：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="password"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">パスワードを再度入力：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="password"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">フルネーム：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="text"
+              placeholder="山田太郎"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">ニックネーム：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="text"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">学校名：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="text"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">学年：</h3>
+          <div className="my-2 mx-5">
+            <select className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none">
+              <option value="">選択してください</option>
+              <option value="1">1年</option>
+              <option value="2">2年</option>
+              <option value="3">3年</option>
+              <option value="4">4年</option>
+            </select>
+          </div>
+
+          <h3 className="ml-5">学部：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="text"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <h3 className="ml-5">学科：</h3>
+          <div className="my-2 mx-5">
+            <input
+              type="text"
+              className="w-full px-2 py-2 bg-white border border-gray-200 rounded-xl text-base appearance-none"
+            />
+          </div>
+
+          <div className="bg-blue-500 hover:bg-blue-600 transition-colors mx-20 rounded-xl py-1 mt-6">
+            <button
+              type="submit"
+              className="text-3xl font-bold w-full text-white cursor-pointer"
+            >
+              登録
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
